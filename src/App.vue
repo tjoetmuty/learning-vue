92441392e6409646425393556e2aa1d12b0b92fb
Tjoet Muty Ahmad <tjmutyahmd27@gmail.com>
2025-01-14 16:44:46 +0800

    api styles: options

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup>
 import MainButton from "./components/MainButton.vue"
+import OptionComponent from "./components/OptionComponent.vue"
 </script>
 
 <template>
   <MainButton/>
+  <div>
+    <OptionComponent/>
+  </div>
 </template>
 
 <style scoped></style>
---
+++ src/components/OptionComponent.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  data(){
+    return{
+      name: "muty learns vue"
+    }
+  },
+  methods: {
+    showName(){
+      console.log("option component result: ", this.name)
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>{{ name }}</h1>
+  <button @click="showName">show name</button>
+</template>
